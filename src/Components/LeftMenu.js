bb6ebe134aa175d39a602ffa3d6dfbe37fb53e7d
JayadevArun <jayadevarun03@gmail.com>
2023-09-26 18:48:45 +0530

    creating left menu bar

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "../Styles/LeftMenu.css";
+
+import {FaSpotify,FaEllipsisH} from "react-icons/fa"
 
 function LeftMenu(){
     return(
-        <div>
+        <div className='leftMenu'>
+            <div className='logoContainer'>
+                <i><FaSpotify /></i>
+                <h2>Spotify</h2>
+                <i><FaEllipsisH /></i>
+                <div className='searchBox'>
 
+                </div>
+            </div>
         </div>
     )
 }
