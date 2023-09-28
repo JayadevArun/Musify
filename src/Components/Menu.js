46cc3b1eeaa36073814be14173f7f549452f2cdf
JayadevArun <jayadevarun03@gmail.com>
2023-09-28 16:50:55 +0530

    set active menulist update

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 
 function Menu({title,menuObject}) {
-  return (
+  useEffect(() => {
+    const allLi=document.querySelector(".MenuContainer ul").querySelectorAll("li");
+
+    function changeMenuActive(){
+        allLi.forEach((n) => n.classList.remove("active"));
+        this.classList.add("active");
+    }
+
+    allLi.forEach((n) => n.addEventListener("click",changeMenuActive))
+  }, []);
+
+    return (
     <div className='MenuContainer'>
         <p className='title'>{title}</p>
 
