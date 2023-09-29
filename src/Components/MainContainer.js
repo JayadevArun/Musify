eb8506f6d7b7c042254698d8ca877cb19c5f81bc
JayadevArun <jayadevarun03@gmail.com>
2023-09-29 22:57:50 +0530

    completed layout,styling of mainmenu

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import "../Styles/MainContainer.css";
 import { Banner } from './Banner';
+import { FaUsers } from 'react-icons/fa';
 
 function MainContainer(){
+
+    useEffect(() => {
+        const allLi=document.querySelector(".menuList").querySelectorAll("li");
+    
+        function changeMenuActive(){
+            allLi.forEach((n) => n.classList.remove("active"));
+            this.classList.add("active");
+        }
+    
+        allLi.forEach((n) => n.addEventListener("click",changeMenuActive))
+      }, []);
+
     return(
         <div className='mainContainer'>
             <Banner />
+
+            <div className='menuList'>
+                <ul>
+                    <li>
+                        <a href='#'>Popular</a>
+                    </li>
+                    <li>
+                        <a href='#'>Albums</a>
+                    </li>
+                    <li>
+                        <a href='#'>Songs</a>
+                    </li>
+                    <li>
+                        <a href='#'>Fans</a>
+                    </li>
+                    <li>
+                        <a href='#'>About</a>
+                    </li>
+                </ul>
+
+                <p>
+                    <i>
+                        <FaUsers />
+                    </i>
+                    12.6M <span>Followers</span>
+                </p>
+            </div>
         </div>
     )
 }
